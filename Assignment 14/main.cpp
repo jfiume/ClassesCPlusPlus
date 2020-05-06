2de2b738fde5f9cdcca723e4ed7545d43ddc7555
Joe Fiume <joefiume88@gmail.com>
2020-05-06 16:08:40 -0700

    Initial Commit

---
+++ Assignment 14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 14
+//
+//  Created by Joe on 5/6/20.
+//  Copyright © 2020 Joe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
